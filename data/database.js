67405e8c4622ae7ff7e76c8fe0719f96bf3be4eb
Hobiana <hobiana.razakanaivo@bva-group.com>
2025-08-11 17:50:47 +0400

    log env

--- data/database.js
+++ data/database.js
@@ -5,7 +5,10 @@ const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
+console.log('process.env: ', process.env);
+
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+console.log('uri: ', uri);
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
