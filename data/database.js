db96e03cad23b32334955c6b5e2814b9baa71d5f
Hobiana <hobiana.razakanaivo@bva-group.com>
2025-08-11 18:22:39 +0400

    fix error db connection

--- data/database.js
+++ data/database.js
@@ -4,13 +4,14 @@ const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
+const appname = process.env.MONGODB_APPNAME;
 
 console.log('clusterAddress: ', clusterAddress);
 console.log('dbUser: ', dbUser);
 console.log('dbPassword: ', dbPassword);
 console.log('dbName: ', dbName);
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=${appname}`;
 console.log('uri: ', uri);
 const client = new MongoClient(uri);
 
