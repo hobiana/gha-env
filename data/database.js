ea7f8e0e32559a278a6a79fab45e9ccbb5fa7473
Hobiana <hobiana.razakanaivo@bva-group.com>
2025-08-11 18:43:49 +0400

    fix error

--- data/database.js
+++ data/database.js
@@ -22,7 +22,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.', err);
+  console.log('Connection failed.', error);
   await client.close();
   console.log('Connection closed.');
 }
